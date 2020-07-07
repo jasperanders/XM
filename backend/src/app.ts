3e2dad0bba96f235b26f165d1fce4d404faf3b51
Jasper Anders <jasper.anders@online.de>
2020-07-07 20:37:37 +0200

    added backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,25 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
+// import { db } from "./models/database";
+import mongoose from "mongoose";
 
-const app = express();
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
+
+app.listen(5000, () => {
+  console.log("server running");
+});
+
+mongoose.connect("mongodb://localhost/xm", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  // we're connected!
+  console.log("connected to db");
+});
+console.log(db);
