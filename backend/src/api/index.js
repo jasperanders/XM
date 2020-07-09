5ec34e343cdeb356586a0dab12477f75c963876e
Jasper Anders <jasper.anders@online.de>
2020-07-09 18:27:29 +0200

    removed old backend

---
+++ backend/src/api/index.js
@@ -0,0 +1,34 @@
+import { Router } from 'express'
+import { env, mongo, port, ip, apiRoot } from '~/config'
+import { doorman } from '~/services/auth/guard'
+/* ENDPOINT_ROUTER_IMPORT */
+import question, { Question } from './question'
+import exam, { Exam } from './exam'
+import auth from './auth'
+import verification from './verification'
+import passwordReset from './password-reset'
+import user, { User } from './user'
+import message, { Message } from './message'
+
+const router = new Router()
+
+/* ENDPOINT_ROUTER_EXPORT */
+router.use('/questions', question)
+router.use('/exams', exam)
+router.use('/auth', auth)
+router.use('/verification', verification)
+router.use('/users', user)
+router.use('/messages', message)
+router.use('/password-reset', passwordReset)
+
+// Export the relevant models for swagger documentation
+export const Models = [
+    /* ENDPOINT_DOCS_EXPORT */
+Question,
+Exam,
+    User,
+    Message
+]
+
+// Export router for Express Server
+export default router
--- backend/src/models/database.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb://localhost:27017", { useNewUrlParser: true });
-
-export const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  // we're connected!
-});
